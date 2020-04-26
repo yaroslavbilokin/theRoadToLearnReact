7e6e828d3d5e84cfd7be5536ae81154dff5501b3
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-04-26 20:39:30 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -20,10 +20,17 @@ const list = [
   },
 ];
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+  }
   render() {
     return (
       <div className="App">
-        {list.map(item => 
+        {this.state.list.map(item => 
           <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
