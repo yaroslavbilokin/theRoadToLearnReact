e44fa242712c60e3b105844cf74c1cbe6718140b
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-04-26 23:07:16 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -19,18 +19,46 @@ const list = [
     objectID: 1,
   },
 ];
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       list,
+      searchTerm: '',
     };
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
   }
+
+
+    onDismiss(id) {
+      const isNotId = item => item.objectID !== id;
+      const updatedList = this.state.list.filter(isNotId);
+      this.setState({
+        list: updatedList
+      })
+    }
+
+    onSearchChange(event) {
+      this.setState({ searchTerm: event.target.value });
+    }
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item => 
+        <form>
+          <input 
+            type="text" 
+            onChange={this.onSearchChange}
+          />
+        </form>
+        
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
           <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -38,6 +66,12 @@ class App extends React.Component {
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                  <button onClick={() => this.onDismiss(item.objectID)}
+                type="button">
+                    Відкинути
+                  </button>
+              </span>
           </div>
          )}
       </div>
