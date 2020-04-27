be4015b96f628eaad00349801d3ab70761e3f95b
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-04-27 22:49:07 +0300

    commit; git commit -m commit

--- src/App.js
+++ src/App.js
@@ -49,35 +49,64 @@ class App extends React.Component {
       this.setState({ searchTerm: event.target.value });
     }
   render() {
+    const {searchTerm, list} = this.state;
     return (
       <div className="App">
-        <form>
-          <input 
-            type="text" 
-            onChange={this.onSearchChange}
-          />
-        </form>
-        
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
-          <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                  <button onClick={() => this.onDismiss(item.objectID)}
-                type="button">
-                    Відкинути
-                  </button>
-              </span>
-          </div>
-         )}
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
+    )
+  }
+}
+
+class Search extends React.Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
     );
   }
 }
 
+class Table extends React.Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type="button"
+              >
+                Отбросить
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
