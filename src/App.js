0e463ab87681fbe7e174c49e561dd160ea734f7d
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-04-29 23:13:57 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -29,27 +16,40 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
 
-
-    onDismiss(id) {
-      const isNotId = item => item.objectID !== id;
-      const updatedList = this.state.list.filter(isNotId);
-      this.setState({
-        list: updatedList
+   onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({
+      list: updatedList
       })
     }
+
+    componentDidMount() {
+      const { searchTerm } = this.state;
+      fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
 
     onSearchChange(event) {
       this.setState({ searchTerm: event.target.value });
     }
     render() {
-      const { searchTerm, list } = this.state;
+      const { searchTerm, result } = this.state;
+
+      if (!result) { return null; }
       return (
         <div className="page">
           <div className="interactions">
@@ -61,7 +61,7 @@ class App extends React.Component {
             </Search>
           </div>
           <Table
-            list={list}
+            list={result.hits}
             pattern={searchTerm}
             onDismiss={this.onDismiss}
           />
