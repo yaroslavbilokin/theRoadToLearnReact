16bf1b0017e5fc2c6993ccfc2f3e285be6d2c963
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-05-01 23:23:39 +0300

    added api

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -37,6 +33,13 @@ class App extends React.Component {
     });
   }
 
+  fetchSearchTopStories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
   componentDidMount() {
       const { searchTerm } = this.state;
       this.fetchSearchTopStories(searchTerm);
@@ -46,16 +49,10 @@ class App extends React.Component {
       this.setState({ searchTerm: event.target.value });
   }
 
-  onSearchSubmit() {
+  onSearchSubmit(event) {
     const { searchTerm } = this.state;
     this.fetchSearchTopStories(searchTerm);
-  }
-
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+    event.preventDefault();
   }
 
   render() {
@@ -68,50 +65,43 @@ class App extends React.Component {
             <Search
               value={searchTerm}
               onChange={this.onSearchChange}
-              onSubmit = {this.onSearchChange}
+              onSubmit = {this.onSearchSubmit}
             >
               Поиск
             </Search>
           </div>
           { result &&
-            <Table
-              list={result.hits}
-              pattern={searchTerm}
-              onDismiss={this.onDismiss}
-            />
-          }
+          <Table
+            list={result.hits}
+            onDismiss={this.onDismiss}
+          />
+        }
         </div>
       );
     }
   }
 
 
-// class Search extends React.Component {
-//   render() {
-//     const { value, onChange, children } = this.props;
-//     return (
-//       <form>
-//         {children} <input
-//           type="text"
-//           value={value}
-//           onChange={onChange}
-//         />
-//       </form>
-//     );
-//   }
-// }
-const Search = ({ value, onChange, children }) =>
-  <form>
-    {children} <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-  </form>
+const Search = ({
+    value,
+    onChange,
+    onSubmit,
+    children
+}) =>
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        {children}
+      </button>
+    </form>
 
-const Table = ({list, pattern, onDismiss}) => 
+const Table = ({ list, onDismiss }) => 
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
@@ -137,31 +127,6 @@ const Table = ({list, pattern, onDismiss}) =>
     )}
   </div>
 
-// class Table extends React.Component {
-//   render() {
-//     const { list, pattern, onDismiss } = this.props;
-//     return (
-//       <div>
-//         {list.filter(isSearched(pattern)).map(item =>
-//           <div key={item.objectID}>
-//             <span>
-//               <a href={item.url}>{item.title}</a>
-//             </span>
-//             <span>{item.author}</span>
-//             <span>{item.num_comments}</span>
-//             <span>{item.points}</span>
-//             <span>
-//             <Button onClick={() => onDismiss(item.objectID)}>
-//                 Отбросить
-//               </Button>
-//             </span>
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
 const Button = ({onClick, className = '', children}) =>
   <button
     onClick={onClick}
@@ -170,26 +135,5 @@ const Button = ({onClick, className = '', children}) =>
     >
     {children}
   </button>
-
-
-// class Button extends React.Component {
-//   render() {
-//     const {
-//       onClick,
-//       className = '',
-//       children,
-//     } = this.props;
-
-//     return (
-//       <button
-//         onClick={onClick}
-//         className={className}
-//         type="button"
-//       >
-//         {children}
-//       </button>
-//     );
-//   }
-// }
 
 export default App;
