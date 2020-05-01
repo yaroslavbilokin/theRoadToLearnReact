00965cc627e9a6ad62229d41ecd7b7ad4a77d581
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-05-01 23:44:57 +0300

    modified pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
+const DEFAULT_HPP = '100';
+const PARAM_HPP = 'hitsPerPage=';
 
 class App extends React.Component {
   constructor(props) {
@@ -48,7 +50,7 @@ class App extends React.Component {
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
