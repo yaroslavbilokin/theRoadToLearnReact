36fb509e57f3a0d2f4b4eff413d390d0709a0d78
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-05-04 22:59:42 +0300

    added error catching

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -72,7 +73,7 @@ class App extends React.Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   }
 
   componentDidMount() {
@@ -100,7 +101,8 @@ class App extends React.Component {
     const {
       searchTerm,
       results,
-      searchKey
+      searchKey,
+      error
     } = this.state;
 
     const page = (
@@ -130,6 +132,15 @@ class App extends React.Component {
              list={list}
               onDismiss={this.onDismiss}
             />
+            { error
+              ? <div className="interactions">
+                <p>Something went wrong.</p>
+              </div>
+              : <Table
+                list={list}
+                onDismiss={this.onDismiss}
+              />
+            }
           <div className="interactions">
             <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
               Больше историй
