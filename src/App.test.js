df21763795ca2665cb66c49628381c571cc8ebce
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-05-11 21:55:01 +0300

    tested Jest

--- src/App.test.js
+++ src/App.test.js
@@ -11,7 +11,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from './App';
+import App, { Search, Button, Table } from './App';
 
 describe('App', () => {
 
@@ -19,6 +19,74 @@ describe('App', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <App />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+describe('Search', () => {
+
+  it('отрисовывает без ошибки', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Поиск</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <Search>Поиск</Search>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+describe('Button', () => {
+
+  it('отрисовывает без ошибки', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Дай мне больше</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <Button>Дай мне больше</Button>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+describe('Table', () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+  };
+
+  it('отрисовывает без ошибки', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props } />, div);
+  });
+
+  test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <Table { ...props } />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
 });
